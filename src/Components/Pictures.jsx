72150bb26d24b12bdc560ac6a5e390b03ad9e609
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-09 11:57:41 -0300

    changed function

--- src/Components/Pictures.jsx
+++ src/Components/Pictures.jsx
@@ -1,35 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Pictures.css'
-function Pictures(card) {
+import { LiaStarSolid } from 'react-icons/lia'
 
-    const state = card.superHost;
-    const stateBed = card.beds;
-    let maxGuests = card.maxGuests;
-
+const pictures = ({ beds, photo, rating, superHost, title, type }) => {
     return (
-        <div className='card'>
-            <div className='img'>
-                <img src={card.photo} className='picture' alt="image"></img>
+        <article>
+            <div className='container'>
+                <img src={photo} alt={title} style={{ width: "100%", height: "100%", borderRadius: "12px" }} />
             </div>
-            <div className='superhost'>
-                {state && (<span className='host'>SUPERHOST</span>)}
-
-                <span className='beds'>
-                    {card.type} {stateBed == null ? "" : `.${card.beds} beds`}
-                </span>
-
-                <div className='star'>
-                    <span className='material-symbols-outlined align-middle' style={{ color: "rgb(235,87,87)" }}>
-                        star
+            <div className='information'>
+                <div className='superhost'>
+                    {superHost ? <span className='host'>SUPERHOST</span> : ""}
+                    <span className='type'>
+                        {type}. {beds} {beds == 0 || beds == 1 ? "bed" : "beds"}
                     </span>
-                    <span className='rating'>{card.rating}</span>
+                </div>
+                <div className='rating'>
+                    <LiaStarSolid
+                        style={{ display: "inline-flex", marginRight: "12px", color: "#eb5757" }} />
+                    <span className='ranking'>{rating}</span>
                 </div>
             </div>
-            <div>
-                <p className='title'>{card.title}</p>
-            </div>
-        </div>
-    )
-}
+            <div style={{ fontWeight: "600", color: "#333333" }}>{title}</div>
+        </article>
+    );
+};
 
-export default Pictures
+export default pictures;
