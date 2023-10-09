fe79fd88e0bc38020cecd00aca3b316150dcaa91
mateus-schiavi <101905382+mateus-schiavi@users.noreply.github.com>
2023-10-09 16:37:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mini_proyecto_nivel2/'
+  base: 'https://github.com/mateus-schiavi/mini_proyecto_nivel2/'
 })
